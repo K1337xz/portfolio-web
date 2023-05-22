bd5eaed9a24d03e7354f3e14a000cfcbf20c987b
Koporson <108631492+K1337xz@users.noreply.github.com>
2023-05-22 19:44:16 +0200

    update

---
+++ src/components/Formrow.jsx
@@ -0,0 +1,18 @@
+export default function FormRow(props) {
+	return (
+		<>
+			<label htmlFor={props.name} className={props.validLabel}>
+				<input
+					type={props.type}
+					value={props.value}
+					name={props.name}
+					onChange={props.handleChange}
+					className={props.class}
+					placeholder={props.placeholder}
+					hidden={props.hidden}
+				/>
+			</label>
+			<span className="error"></span>
+		</>
+	);
+}
--- src/components/contactform/Contactform.jsx
+++ src/components/contactform/Contactform.jsx
@@ -1,10 +1,40 @@
 import "./_Contactform.scss";
+import FormRow from "../Formrow";
 
 export default function Contactform() {
 	return (
 		<div className="contact">
 			<div className="contact__top">
 				<h3>Contact with me</h3>
+			</div>
+			<div className="contact__content">
+				<form className="contact__form">
+					<FormRow
+						type="text"
+						name="contactName"
+						placeholder="Name"
+					/>
+					<FormRow
+						type="text"
+						name="contactLastName"
+						placeholder="Last Name"
+					/>
+					<FormRow
+						type="email"
+						name="contactEmail"
+						placeholder="Email"
+					/>
+					<FormRow
+						type="text"
+						name="contactPhone"
+						placeholder="Phone Number"
+					/>
+					<textarea
+						placeholder="Message"
+						style={{ resize: "none" }}
+					></textarea>
+					<button>Submit Now</button>
+				</form>
 			</div>
 		</div>
 	);
